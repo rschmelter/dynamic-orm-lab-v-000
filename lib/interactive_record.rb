1d5f4cb24af95558f09741dc84e0aa345c34e8d6
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-11 21:55:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
     key = hash.keys
     value = hash.values
     sql = "SELECT * FROM #{self.table_name} WHERE '#{key}' = '#{value}'"
+     DB[:conn].execute(sql)
 
 
   end
